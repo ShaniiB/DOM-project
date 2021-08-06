78e4161c247614ed82a611c38a6cf28497cb51b9
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-08-06 10:57:02 -0700

    prompt

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //alert("Sanity Test");
 
 //make container
-function createContainer(); {
+function createContainer() {
 
 let mainContainer = document.getElementById("mainContainer");    
 let headerOne = document.createElement("h1");
@@ -32,33 +32,35 @@ li.appendChild(listText);
 ordered.appendChild(li);
 mainContainer.appendChild(ordered);
 
-}
+
 
 // prompt for date
-let currentDate = prompt("What is today?");
+let currentDate = prompt("What is today?\n\nMM/DD/YYYY")
 while (currentDate === "" || currentDate === null) { // user must answer to move on.
 currentDate = prompt("What todays date?");
 }
+console.log(currentDate)
 
 // prompt for confidence
-let confidenceLevel = prompt("How're you feeling today?\n\nLow / Medium / High?");
+let confidenceLevel = prompt("How're you feeling today?\n\nLow / Medium / High?")
 while (confidenceLevel === "" || confidenceLevel === null) { // user must answer to move on.
 confidenceLevel = prompt("Speak to me?");
 }
+console.log(confidenceLevel)
 
 // prompt for journal
-let journalInput = prompt("What's on your mind?");
+let journalInput = prompt("What's on your mind?")
 while (journalInput === "" || journalInput === null ) { // user must answer to move on.
 journalInput = prompt("Reflection");
 }
-
+console.log(journalInput);
 
 // array for journal entry
 //let journalEntry = []
 
 
 //class {"journalEntry"} {
-   create(currentdate, journalinput, confidencelevel); {
+//create(currentdate, journalinput, confidencelevel); {
 
 //this.currentdate = currentdate;
 //this.journalinput = journalinput;
