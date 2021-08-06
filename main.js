778f55ba2018e7a43bad45a4e33ebc8d3b4042f2
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-08-05 19:08:55 -0700

    more fixing

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 "use strict"
 //alert("Sanity Test");
+
 //make container
 function createContainer() {
+
+// array for journal entry
+let journalEntry = [];
+
+
+class journalEntry {
+   create(currentdate, journalinput, confidencelevel){
+
+this.currentdate = currentdate;
+this.journalinput = journalinput;
+this.confidencelevel = confidencelevel;
+
+}
+}
 
 let mainContainer = document.getElementById("mainContainer");    
 let headerOne = document.createElement("h1");
@@ -9,11 +24,46 @@ let text = document.createTextNode("Hello");
 headerOne.appendChild(text);
 console.log(mainContainer);
 mainContainer.appendChild(headerOne);
-console.log("im here"); 
+console.log("sanity test"); 
+
+// unordered lists
+
+let unordered = document.createElement("ul");
+unordered.classList.add("unordered");
+let listItems = document.createElement("li");
+unordered.appendChild(listItems);
+let ulText = document.createTextNode("Hello");
+listItems.appendChild(ulText);
+mainContainer.appendChild(unordered);
+
+
+// ordered lists
+let ordered = document.createElement("ol");
+let li = document.createElement("li")
+ordered.classList.add("ordered");
+let listText = document.createTextNode("Hello");
+li.appendChild(listText);
+ordered.appendChild(li);
+mainContainer.appendChild(ordered);
+
+
+// prompt for 
+let reflection = prompt("Whomp-whomp");
+while (reflection === "" || reflection === null) { // user must answer to move on.
+reflection = prompt("Speak to me.");
+}
+
+// prompt for
+let todayDate = prompt("Whomp");
+while (todayDate === "" || todayDate === null) { // user must answer to move on.
+todayDate = prompt("Speak to me.");
+}
+
+// prompt fot
+let confidenceTest = prompt("Whomp");
+while (confidenceTest === "" || confidenceTest === null) { // user must answer to move on.
+confidenceTest = prompt("Speak to me.");
+}
 
 }
 createContainer();
-
-//ask name for journal
-//ask for date for journal
-// insert journal input
