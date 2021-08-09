0e2e095cad902bab309f135ee9fd826b77f4ba71
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-08-08 19:55:07 -0700

    updating

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createHolder() { //____MAKING A MORE ACCEPTABLE BODY
     document.getElementById("holder").style.borderRadius = "15px";
 
 };
-class Journal {
+class Journal { //____INPUT FOR THE USERS & ARRAY PUSHER
     constructor(currentDate, confidenceLevel, journalInput) {
 
         this.currentDate = currentDate;
@@ -33,18 +33,44 @@ class Journal {
 };
 //_____STARTING JOURNAL ENTRY & FUNCTIONALITY
 function createJournals() {
-
+   
     let containJournal = document.getElementById("containJournal");
     containJournal.append;
 
-    let currentDate = prompt ("Help");
-    let confidenceLevel = prompt ("Why");
-    let journalInput = prompt ("Is");
+    let currentDate = prompt ("Please enter today's date?");
+    let confidenceLevel = prompt ("How confident are you feeling today?");
+    let journalInput = prompt ("Enter your what's on your mind?");
 
     if (currentDate != null && confidenceLevel != null && journalInput != null) {
-        alert("Good job, champ");
+        alert("Good job, champ!");
+}
 
-};
+    else{ 
+     alert("This is a safe space.");
+     createJournals();
+    
+ };
+
+    let unordered = document.createElement("ul");   
+    let listItem = document.createElement("li");
+    let liText = document.createTextNode(currentDate);
+    listItem.appendChild(liText);
+    unordered.appendChild(listItem);
+    holder.appendChild(unordered);
+
+    let unordered1 = document.createElement("ul");   
+    let listItem1 = document.createElement("li");
+    let liText1 = document.createTextNode(`Confidence level is ${confidenceLevel}`);
+    listItem1.appendChild(liText1);
+    unordered1.appendChild(listItem1);
+    holder.appendChild(unordered1);
+
+    let unordered2 = document.createElement("ul");   
+    let listItem2 = document.createElement("li");
+    let liText2 = document.createTextNode(journalInput);
+    listItem2.appendChild(liText2);
+    unordered2.appendChild(listItem2);
+    holder.appendChild(unordered2);
 
     let newEntry = new Journal;
     
